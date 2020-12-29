189af230cd2777e39361725c0d5da4f77bb219e9
SUTAKURE <udonnamerou@gmail.com>
2020-12-29 23:10:06 +0900

    多項式回帰式の出力

--- sample.py
+++ sample.py
@@ -1,40 +1,11 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_boston
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
-<< << << < HEAD
 
-boston = load_boston()
+x = np.array([-1.2, -0.6, 0.0, 0.6, 1.2, 1.8, 2.4, 3.0, 3.6, 4.2])
+y = np.cos(x)
 
-x = boston.data
-y = boston.target
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
-
-model = LinearRegression()
-model.fit(x_train, y_train)
-
-model1 = Lasso()
-model1.fit(x_train, y_train)
-
-model2 = Ridge()
-model2.fit(x_train, y_train)
-
-pred = model.predict(x_test)
-pred1 = model1.predict(x_test)
-pred2 = model2.predict(x_test)
-
-print("LinearRegression: ", r2_score(y_test, pred))
-print("Lasso: ", r2_score(y_test, pred1))
-print("Ridge: ", r2_score(y_test, pred2))
-
-plt.plot(model.predict(x_test), linestyle="solid",
-         color="red", label="lr")
-plt.plot(model1.predict(x_test), linestyle="solid",
-         color="green", label="lasso")
-plt.plot(model2.predict(x_test), linestyle="solid",
-         color="blue", label='ridge')
-plt.title("LinearRegression, Lasso, Ridge")
-plt.legend()
-plt.show()
+# 3次元（y=a0+a1x1+a2x^2+a3x^3)まで考えた回帰分析
+z = np.polyfit(x, y, 3)
+print(z)
+p = np.poly1d(z)
+print(p)
